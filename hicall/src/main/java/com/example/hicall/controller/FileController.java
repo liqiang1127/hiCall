6b0e9e0eecde6ed62eacdc4b7ea0aec752555c3f
liqiang <565384027@qq.com>
2019-07-02 17:10:46 +0800

    fileDownload

--- hicall/src/main/java/com/example/hicall/controller/FileController.java
+++ hicall/src/main/java/com/example/hicall/controller/FileController.java
@@ -1,20 +1,26 @@
 package com.example.hicall.controller;
 
 import com.example.hicall.bean.HiCallResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
 @EnableAutoConfiguration
 @RestController
-public class UploadController {
+public class FileController {
     private String path = "F:/log";
+
     @RequestMapping("/upload")
     public HiCallResponse fileUpload(@RequestParam("file")MultipartFile file){
         HiCallResponse res = new HiCallResponse();
@@ -51,4 +57,45 @@ public class UploadController {
         res.getInfo().put("files",fileNames);
         return res;
     }
+
+    @Autowired
+    HttpServletResponse response;
+    @RequestMapping("/down")
+    public HiCallResponse down(@RequestParam("fileName") String fileName){
+        HiCallResponse res = new HiCallResponse();
+        File file = new File(path + "/" +fileName);
+        if(!file.exists()){
+            res.setErrorNo("-1");
+            res.setMessage("file does`t exists");
+            return res;
+        }
+        response.setContentType("application/force-download");
+        response.setHeader("Content-Disposition", "attachment;fileName="+fileName);
+
+        byte[] buffer = new byte[1024];
+        FileInputStream fis;
+        BufferedInputStream bis;
+
+        OutputStream os;
+
+        try {
+            os = response.getOutputStream();
+            fis = new FileInputStream(file);
+            bis = new BufferedInputStream(fis);
+            int i = bis.read(buffer);
+            while( i != -1){
+                os.write(buffer);
+                i = bis.read(buffer);
+            }
+            bis.close();
+            fis.close();
+        }catch (Exception e){
+            res.setErrorNo("-1");
+            res.setMessage(e.getMessage());
+            return res;
+        }
+        return null;
+    }
+
+
 }
